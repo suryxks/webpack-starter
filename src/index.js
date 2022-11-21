77ff846c4435bbf470d46d41924efe7e8efea60a
Surya <123004250@sastra.ac.in>
2022-11-21 19:01:13 +0530

    feat: added css and asset(image and font) loaders

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './react-js-icon.svg';
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+  
+   // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  myIcon.style.maxWidth = '50px';
+  element.appendChild(myIcon);
   return element;
 }
 
