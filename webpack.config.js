a280386c61e40e4d5d713c58dcd4161aa909c6f7
Surya <123004250@sastra.ac.in>
2022-11-21 18:37:39 +0530

    feat: added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,12 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path:path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
-        rules: [
-          {
+        rules: [{
             test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
-      },
-}
+            use:['style-loader','css-loader'],
+        }]
+    }
+};
