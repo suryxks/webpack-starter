a0d5d14abb48c55a4e7b03bf6aaa8a895ea39342
Surya <123004250@sastra.ac.in>
2022-11-29 07:40:42 +0530

    feat: added development mode

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './react-js-icon.svg';
+import printMe  from './print';
 function component() {
   const element = document.createElement('div');
-
+  const btn = document.createElement('button');
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
   
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
    // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Icon;
   myIcon.style.maxWidth = '50px';
   element.appendChild(myIcon);
+  element.appendChild(btn);
   return element;
 }
 
